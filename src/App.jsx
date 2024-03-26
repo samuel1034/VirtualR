e2e2c1f468042b85e164a8ee17072177d73ac03b
Samuel Fuentes (Genius) <samuelgenius1034@gmail.com>
2024-03-26 15:05:29 -0400

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import FeatureSection from './components/FeatureSection'
+import Footer from './components/Footer'
+import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Workflow from './components/Workflow'
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Navbar/>
+      <div className='max-w-7xl mx-auto pt-20 px-6'>
+        <HeroSection/>
+        <FeatureSection/>
+        <Workflow/>
+        <Pricing/>
+        <Testimonials/>
+        <Footer/>
+      </div>
+
+
     </>
-    
+
   )
 }
 
